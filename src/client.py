0f82f67d67a927b0c066f1720fb27d254588f4c3
Dennis Pfleger <dennis.pfleger@web.de>
2019-08-23 02:18:06 +0200

    Client args to match address convention (IP:PORT) instead of passing address and port as two seperate args

--- src/client.py
+++ src/client.py
@@ -1,12 +1,13 @@
 import socket
 import sys 
 
-HOST = sys.argv[1]
-PORT = (int)(sys.argv[2])
-msg = sys.argv[3].encode()
+args = sys.argv[1].split(':')
+host = args[0]
+port = (int)(args[1])
+msg = sys.argv[2].encode()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect((host, port))
     s.sendall(bytes(msg))
     data = s.recv(1024)
 
