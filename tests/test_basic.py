8a861f4c90c6808ca53e5de06d9010885d9ea9a2
Dennis Pfleger <dennis.pfleger@web.de>
2019-08-25 00:28:17 +0200

    Add basic server startup test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,8 @@
 import pytest
+import socket
 
-def test_basic():
-    assert(1==1)
+def test_start_server(HOST, PORT): 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        assert(s is not None)
