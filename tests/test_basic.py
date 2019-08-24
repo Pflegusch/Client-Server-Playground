66f871f79e9ddce5bac69cbea33c1ebf7f0a376d
Dennis Pfleger <dennis.pfleger@web.de>
2019-08-25 00:31:28 +0200

    host=localhost, port=65333

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,7 @@
 import pytest
 import socket
 
-def test_start_server(HOST, PORT): 
+def test_start_server(HOST='localhost', PORT=65333): 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
