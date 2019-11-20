cdf084aa259e09418c4b512633e0034f3748edb1
Dennis Pfleger <dennis.pfleger@web.de>
2019-11-20 22:00:39 +0100

    1 instead of 0 sys arg

--- src/client.py
+++ src/client.py
@@ -5,10 +5,10 @@ import sys
 # host = args[0]
 # port = (int)(args[1])
 # msg = sys.argv[2].encode()
-msg = sys.argv[0].encode()
+msg = sys.argv[1].encode()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(("192.168.178.31", 33333))
+    s.connect(('192.168.178.31', 33333))
     s.sendall(bytes(msg))
     data = s.recv(1024)
 
