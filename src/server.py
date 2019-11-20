e14aa9f5868398be01bf42d3dc48022b23327254
Dennis Pfleger <dennis.pfleger@web.de>
2019-11-20 21:11:13 +0100

    Update server

--- src/server.py
+++ src/server.py
@@ -6,6 +6,7 @@ import sys
 
 HOST = '192.168.178.31'  # Standard loopback interface address (localhost)
 PORT = (int)(sys.argv[1])
+TIMEOUT = 15
 
 def adjust_volume(program, data): 
     import re
@@ -26,27 +27,26 @@ while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-        print("Server started on " + str(HOST) + ":" + str(PORT))
+        #s.settimeout(TIMEOUT)
+        print("Server started on " + str(HOST) + ":" + str(PORT)) # + " with timeout " + str(TIMEOUT) + "s")
         conn, addr = s.accept()
         with conn:
             print('Connected by', addr)
-            while True:               
+            while True:              
                 data = conn.recv(1024)
                 if data == b'exit': 
                     conn.sendall(b'Server shutdown')
                     print("Client initiated shutdown")
-                    exit() 
                 elif data == b'sleep': 
                     os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
                     conn.sendall(b'Server set to sleep')
-                    exit()
                 elif b'vol'in data:
                     try: 
                         adjust_volume("Spotify", data)
                         conn.sendall(b'Master volume successfully adjusted!')
                     except: 
                         print("Unknown Error occurred") 
-                        conn.sendall(b'Unknown error occured') 
-                        exit() 
+                        conn.sendall(b'Unknown error occured')  
                 if not data:
                     break
+                print("\n")
