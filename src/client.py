d01baeb005bf3585f78f42618412b79b4f0c3f03
Dennis Pfleger <dennis.pfleger@web.de>
2019-11-20 21:53:52 +0100

    Default stuff fow now

--- src/client.py
+++ src/client.py
@@ -1,13 +1,14 @@
 import socket
 import sys 
 
-args = sys.argv[1].split(':')
-host = args[0]
-port = (int)(args[1])
-msg = sys.argv[2].encode()
+# args = sys.argv[1].split(':')
+# host = args[0]
+# port = (int)(args[1])
+# msg = sys.argv[2].encode()
+msg = sys.argv[0].encode()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, port))
+    s.connect(("192.168.178.31", 33333))
     s.sendall(bytes(msg))
     data = s.recv(1024)
 
